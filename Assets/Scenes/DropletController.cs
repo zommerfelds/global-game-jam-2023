93dd940c9ca546123c826a466cfac17bd456c28d
Sumit Sinha <sumitsinha@Sumits-MacBook-Pro.local>
2023-02-03 21:02:01 +0100

    Moving the ball left and right using the A and D keys

---
+++ Assets/Scenes/DropletController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropletController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(0.1f, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-0.1f, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0.0f, 0f, -0.1f);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0.0f, 0f, 0.1f);
+        }
+    }
+}
