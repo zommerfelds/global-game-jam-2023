a221028da7d835aff58a5986ce03bf011d2ca83d
Sumit Sinha <isumit4u@gmail.com>
2023-02-04 15:56:55 +0100

    Minor update in player controller.

--- Assets/Scenes/DropletController.cs
+++ Assets/Scenes/DropletController.cs
@@ -14,6 +14,7 @@ public class DropletController : MonoBehaviour
 
     private float velX = 0.0f;
     private float velXDelta = 0.02f;
+    private int nextNarrationTextIndex = 1;
 
     private static float VEL_X_MAX = 0.2f;
 
@@ -26,12 +27,19 @@ public class DropletController : MonoBehaviour
 
         if (RestartGame())
         {
-            Time.timeScale = 1;
-
             score = 0;
+            nextNarrationTextIndex += 1;
+            narrationArea.SetActive(true);
+            gameOverScreen.SetActive(false);
+
+            ShowLevel1Story();
+        }
+
+        if (nextNarrationTextIndex > 3)
+        {
+            Time.timeScale = 1;
             gamePaused = false;
             narrationArea.SetActive(false);
-            gameOverScreen.SetActive(false);
         }
     }
 
@@ -59,6 +67,8 @@ public class DropletController : MonoBehaviour
         {
             gameOverScreen.SetActive(true);
             Time.timeScale = 0;
+
+            nextNarrationTextIndex = 0;
             gamePaused = true;
         }
     }
@@ -85,15 +95,50 @@ public class DropletController : MonoBehaviour
         }
 
         velX = Mathf.Clamp(velX, -VEL_X_MAX, VEL_X_MAX);
+
+        //Vector3 newPos = transform.position + new Vector3(velX, 0, 0);
+        //transform.position = Vector3.Lerp(
+        //    transform.position,
+        //    newPos,
+        //    Time.deltaTime * 50);
         transform.Translate(velX, 0f, 0f);
     }
 
     private void Animate()
     {
+        //dropImage.transform.rotation = Quaternion.Lerp(
+        //    dropImage.transform.rotation,
+        //    Quaternion.Euler(0, 0, 0 - velX / VEL_X_MAX * 30 // face in the direction of speed
+        //    + Mathf.Cos(Time.time * 50) * 5 // wiggle a bit
+        //), Time.deltaTime * 10);
+
         dropImage.transform.rotation = Quaternion.Euler(0, 0, 0
             - velX / VEL_X_MAX * 30 // face in the direction of speed
             + Mathf.Cos(Time.time * 30) * 5 // wiggle a bit
         );
+    }
+
+    private void ShowLevel1Story()
+    {
+        Debug.Log("Story Page: " + nextNarrationTextIndex);
+        if (nextNarrationTextIndex == 1)
+        {
+            storyText.text = "Lorem Ipsum is simply dummy text of the "+
+                "printing and typesetting industry. Lorem Ipsum has been the " +
+                "industry's standard dummy text ever since the 1500s, when " +
+                "an unknown printer took a galley of type and scrambled it " +
+                "to make a type specimen book. It has survived not only five " +
+                "centuries, but also the leap into electronic typesetting, " +
+                "remaining essentially unchanged.";
+        }
+        else if (nextNarrationTextIndex == 2)
+        {
+            storyText.text = "Put simply, changing the time scale from its " +
+                "default of one will speed up or slow the game down ? for " +
+                "example, you can run the game at half speed with a time " +
+                "scale of 0.5, or twice as fast with a timescale of 2). " +
+                "Setting it to zero, pauses the game entirely.";
+        }
     }
 
     private bool RestartGame()
